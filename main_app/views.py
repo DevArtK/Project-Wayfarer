36742117a6c9ff4e8e4ff6ec52647040222489e1
Art <alphaunionxt@gmail.com>
2020-08-05 08:56:00 -0400

    basic urls,views, templates, static, css

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+# from .models import
+# from .forms import
+
+
+# ----- HOME Route -----
+def home(request):
+    return render(request, "home.html")
+
+
+# ----- ABOUT Route -----
+def about(request):
+    return render(request, "about.html")
+
+
+# ----- LOGIN Roue -----
+def login(request):
+    return render(request, "login.html")
+
+
+# ----- SIGNUP Route -----
+def signup(request):
+    return render(request, "signup.html")
+
+
+# -----  -----
+
+
+# -----  -----
+
+
+# -----  -----
