202868c327a734269bf0381485c7debd50325b76
zetadx9 <beatsrus44_44@yahoo.com>
2020-08-05 12:59:25 -0400

    City Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+
+# City Model
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    # image = models.ImageField(upload_to="images/")#
+    
+
