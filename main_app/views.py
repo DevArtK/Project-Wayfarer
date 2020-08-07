3030261658b2090a95103146dfe39e5e5e0f767f
Art <alphaunionxt@gmail.com>
2020-08-07 18:26:55 -0400

    user edit started

--- main_app/views.py
+++ main_app/views.py
@@ -7,20 +7,7 @@ from .forms import RegistrationForm, ProfileForm
 from django.contrib.auth.models import User
 
 
-class City:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-
-
-City = [
-    City("Atlanta", "GA"),
-    City("Norwalk", "CT"),
-    City("Brooklyn", "NY"),
-]
-
 # ----- User Reg + User Profile
-
 
 class ProfilView(UpdateView):
     model = ProfileForm
@@ -87,51 +74,6 @@ def user_detail(request, user_id):
 
     }
     return render(request, 'user/detail.html', context)
-
-# CITIES = (
-
-#     ('Austin, TX')
-#     'Jacksonville, FL'
-#     'Fort Worth, TX'
-#     'San Francisco, CA'
-#     'Columbus, OH'
-#     'Charlotte, NC'
-#     'Indianapolis, IN'
-#     'Seattle, WA'
-#     'Denver, CO'
-#     'Washington, DC'
-#     'El Paso, TX'
-#     'Boston, MA '
-#     'Nashville, TN '
-#     'Portland, OR'
-#     'Las Vegas, NV'
-#     'Detroit, MI'
-#     'Oklahoma City, TN'
-#     'Memphis, TN'
-#     'Louisville, KY'
-#     'Baltimore, MD'
-#     'Milwaukee, WI'
-#     'Albuquerque, NM'
-#     'Tucson, AZ'
-#     'Fresno, CA'
-#     'Sacramento, CA '
-#     'Mesa, AZ'
-#     'Atlanta, GA'
-#    'Kansas City,'
-#     'Colorado Springs,'
-#     'Miami, FL'
-#     'Raleigh, NC'
-#     'Long Beach, CA'
-#     'Virginia Beach, VA'
-#     'Omaha, NE'
-#     Oakland, CA
-#     'Minneapolis, MN'
-#     'Arlington, TX '
-#     'Tampa, FL'
-#     'Tulsa, OK'
-#     'New Orleans,'
-
-# )
 
 
 def post_index(request):
